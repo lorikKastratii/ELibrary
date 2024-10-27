09ed2a32dd5d502de263c89ed9a820da9f843baf
lorikKastratii <63415954+lorikKastratii@users.noreply.github.com>
2024-10-27 10:09:28 +0100

    feat: continued adding more tests for OrderService

--- ELibrary.Orders/ELibrary.Orders.Tests/Tests/OrderServiceTests.cs
+++ ELibrary.Orders/ELibrary.Orders.Tests/Tests/OrderServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using ELibrary.Orders.Tests.Builders;
+﻿using ELibrary.Orders.Application.Common.Errors;
+using ELibrary.Orders.Tests.Builders;
 using ELibrary.Orders.Tests.Helpers;
 using FluentAssertions;
 
@@ -34,6 +35,60 @@ namespace ELibrary.Orders.Tests.Tests
                 .VerifyGetBookIsCalled((byte)request.OrderItems.Count)
                 .VerifyGetUserByIdIsCalled()
                 .VerifyOrderRepositoryIsCalled()
+                .VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public async Task CreateOrderAsync_ShouldReturnFailure_WhenUserIsNull()
+        {
+            //Arrange
+            var request = OrderServiceHelper.GenerateOrderRequest(UserId);
+
+            var builder = new OrderServiceBuilder()
+                .WithOrderRepository()
+                .WithGetUserById(UserId, null);
+
+            var sut = builder.Build();
+
+            //Act
+            var result = await sut.CreateOrderAsync(request);
+
+            //Assert
+            result.IsSuccess.Should().Be(false);
+            result.Data.Should().BeNull();
+            result.Error?.Equals(OrderErrors.UserNotFound);
+
+            builder
+                .VerifyGetUserByIdIsCalled()
+                .VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public async Task CreateOrderAsync_ShouldReturnFailure_WhenStockIsNotAvailable()
+        {
+            //Arrange
+            var request = OrderServiceHelper.GenerateOrderRequest(UserId);
+            var user = OrderServiceHelper.GenerateUser();
+            var book = OrderServiceHelper.GenerateBook(stock: 0);
+
+            var builder = new OrderServiceBuilder()
+                .WithOrderRepository()
+                .WithGetBook(book)
+                .WithGetUserById(UserId, user);
+
+            var sut = builder.Build();
+
+            //Act
+            var result = await sut.CreateOrderAsync(request);
+
+            //Assert
+            result.IsSuccess.Should().Be(false);
+            result.Data.Should().BeNull();
+            result.Error?.Equals(OrderErrors.StockEmpty);
+
+            builder
+                .VerifyGetUserByIdIsCalled()
+                .VerifyGetBookIsCalled((byte)request.OrderItems.Count)
                 .VerifyNoOtherCalls();
         }
     }

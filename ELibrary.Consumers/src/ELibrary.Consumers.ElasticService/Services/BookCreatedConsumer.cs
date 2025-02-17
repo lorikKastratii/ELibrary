445d39ed996ef9a194ebfb77cc21d29cdf24daa8
lorikKastratii <63415954+lorikKastratii@users.noreply.github.com>
2025-02-17 22:34:25 +0100

    feat: add Shared Library

---
+++ ELibrary.Consumers/src/ELibrary.Consumers.ElasticService/Services/BookCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using ELibrary.Consumers.ElasticService.Events;
+using MassTransit;
+
+namespace ELibrary.Consumers.ElasticService.Services
+{
+    public class BookCreatedConsumer : IConsumer<BookCreated>
+    {
+        private readonly ILogger<BookCreatedConsumer> _logger;
+        public BookCreatedConsumer(ILogger<BookCreatedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<BookCreated> context)
+        {
+            _logger.LogInformation("Received BookCreated event with BookId: {BookId}", context.Message.Id);
+            // You can add additional logic here (e.g., updating a database, calling another service, etc.)
+            return Task.CompletedTask;
+        }
+    }
+}

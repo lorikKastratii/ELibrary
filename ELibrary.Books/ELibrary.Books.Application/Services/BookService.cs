6a8011fa674fe0071cf77a7076cad08a3f54bde1
lorikKastratii <63415954+lorikKastratii@users.noreply.github.com>
2025-02-17 22:32:27 +0100

    feat: add Elastic Service support and resolve conflicts

--- ELibrary.Books/ELibrary.Books.Application/Services/BookService.cs
+++ ELibrary.Books/ELibrary.Books.Application/Services/BookService.cs
@@ -7,7 +7,9 @@ using ELibrary.Books.Application.Extensions.Errors;
 using ELibrary.Books.Application.Interfaces;
 using ELibrary.Books.Domain.Exceptions.Book;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
+using Nest;
+using MassTransit;
+using ELibrary.Books.Application.Events;
 
 namespace ELibrary.Books.Application.Services
 {
@@ -17,13 +19,18 @@ namespace ELibrary.Books.Application.Services
         private readonly IAuthorRepository _authorRepository;
         private readonly IMapper _mapper;
         private readonly ILogger<BookService> _logger;
+        private readonly IElasticSearchService _elasticSearchService;
+        private readonly IBus _bus;
 
-        public BookService(IBookRepository bookRepository, IMapper mapper, ILogger<BookService> logger, IAuthorRepository authorRepository)
+        public BookService(IBookRepository bookRepository, IMapper mapper, ILogger<BookService> logger,
+            IAuthorRepository authorRepository, IElasticSearchService elasticSearchService, IBus bus)
         {
             _bookRepository = bookRepository;
             _mapper = mapper;
             _logger = logger;
             _authorRepository = authorRepository;
+            _elasticSearchService = elasticSearchService;
+            _bus = bus;
         }
 
         public async Task<ServiceResponse<List<BookDto>>> GetBooksAsync(CancellationToken cancellationToken)
@@ -63,6 +70,8 @@ namespace ELibrary.Books.Application.Services
             {
                 return new ServiceResponse<BookDto>(BookErrors.BOOK_CREATION_ERROR);
             }
+
+            await _bus.Publish(new BookCreated { Id = book.Id});
 
             return new ServiceResponse<BookDto>(bookDto);
         }
